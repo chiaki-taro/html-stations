29e7f33bdef8edce2b39387dcee26b2d9f093fd7
“Chiaki” <chias.926@gmail.com>
2023-09-01 23:34:30 +0900

    station14 clear

--- src/scripts/station14.js
+++ src/scripts/station14.js
@@ -6,10 +6,14 @@ function getData() {
         { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
         { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
     ];
-    return test.map(buildFullName)
+    return test.map(buildFullName);
 }
 
 function buildFullName(data) {
+    const full_name = data.family_name + ' ' + data.first_name;
+    let newdata = data
+    newdata.full_name=full_name
+    return newdata;
     // Station14の問題はこちらです。想定する答えをここを書いてください。
     // 氏名がわかるようにしつつ、半角スペースで繋いでください。
     // またtest配列のそれぞれのオブジェクトに対して、full_nameのプロパティが追加されるように実装すること
